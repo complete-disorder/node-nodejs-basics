03127fb72437f864ba62beda3abb43b72ffa5c28
vitalii-bakov <bv@subtotal.ru>
2024-10-07 23:46:49 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,60 @@
+import fs from "fs/promises";
+import path from "path";
+
+import * as url from "url";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const SOURCES_FOLDER_NAME = "files";
+
+const BASE_FILE_NAME = "fresh";
+const FILE_EXTENSION = ".txt";
+
+const FILE_NAME = `${BASE_FILE_NAME}${FILE_EXTENSION}`
+
+const getFilePath = (fileName = '', folderName = SOURCES_FOLDER_NAME) => {
+    return path.join(__dirname, folderName, fileName);
+}
+
+const filePath = getFilePath(FILE_NAME);
+
+const FILE_CONTENT = "I am fresh and young";
+
+const EEXIST_ERROR_CODE = 'EEXIST'
+
+class FsOperaionError extends Error {
+    constructor(originalError){
+        super("FS operation failed")
+        this.name = "FsOperaionError"
+        this.originalError = originalError
+    }
+}
+
+class UnexpectedError extends Error {
+    constructor(originalError){
+        super("Unexpected error occurred")
+        this.name = "UnexpectedError"
+        this.originalError = originalError
+    }
+}
+
+
 const create = async () => {
-    // Write your code here 
+
+    try {
+ 
+        await fs.writeFile(filePath, FILE_CONTENT, { flag: "wx" });
+
+    }
+
+    catch(error) {
+
+        if (error.code === EEXIST_ERROR_CODE) {
+            throw new FsOperaionError(error)
+        }
+
+        throw new UnexpectedError()
+    }
 };
 
 await create();
