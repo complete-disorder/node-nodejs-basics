9769d311fedfceb4221ea27561a61425c8786dea
vitalii-bakov <bv@subtotal.ru>
2024-10-08 01:48:58 +0300

    feat: add old read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,35 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+import { FsOperaionError } from "../custom-errors.js";
+
+import {
+    SOURCES_FOLDER_NAME,
+    FILE_TO_READ,
+    ERROR_EVENT,
+  } from "../constants.js";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const { stdout } = process;
+
+const FILE_TO_READ_PATH = path.join(
+    __dirname,
+    SOURCES_FOLDER_NAME,
+    FILE_TO_READ
+  );
+
+
+  export const throwEror = () => {
+    throw new FsOperaionError();
+  };
+  
+
+
 const read = async () => {
-    // Write your code here 
+    const readStream = createReadStream(FILE_TO_READ_PATH);
+    readStream.on(ERROR_EVENT, throwEror);
+    readStream.pipe(stdout);
 };
 
 await read();
