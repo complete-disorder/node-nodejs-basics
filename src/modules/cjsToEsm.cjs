38646f5e1e67e2e950fda957d982e50dfacf63af
vitalii-bakov <bv@subtotal.ru>
2024-10-08 02:00:42 +0300

    feat: add old modules task

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,16 +1,24 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from "path";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import "./files/c.js";
 
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const require = createRequire(__filename);
+
+const PORT = 3000;
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+  unknownObject = require("./files/a.json");
 } else {
-    unknownObject = require('./files/b.json');
+  unknownObject = require("./files/b.json");
 }
 
 console.log(`Release ${release()}`);
@@ -21,20 +29,17 @@ console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
 const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
+  res.end("Request accepted");
 });
-
-const PORT = 3000;
 
 console.log(unknownObject);
 
 myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
+export default {
+  unknownObject,
+  myServer,
 };
-
