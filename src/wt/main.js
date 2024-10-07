47f9f3b90e8d958ee191f46f6a0a07e2476a3e62
vitalii-bakov <bv@subtotal.ru>
2024-10-08 01:51:33 +0300

    feat: add old worker solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { Worker } from "worker_threads";
+import { fileURLToPath } from "url";
+import path from "path";
+import os from "os";
+
+const { length: numberOfCores } = os.cpus();
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const WORKER_PATH = path.join(__dirname, "worker.js");
+
+const runService = (workerData) =>
+  new Promise((resolve, reject) => {
+    const worker = new Worker(WORKER_PATH, {
+      workerData,
+    });
+    worker.postMessage(workerData);
+    worker.on("message", resolve);
+    worker.on("error", reject);
+  });
+
 const performCalculations = async () => {
-    // Write your code here
+  try {
+    const promises = Array(numberOfCores)
+      .fill(10)
+      .map((num, index) => runService(num + index));
+    const results = await Promise.all(promises);
+    console.log(results);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-await performCalculations();
+performCalculations();
